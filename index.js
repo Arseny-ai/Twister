4ae896ad0c4980b83f23ac2281309d0f63cb89d9
Arseny <lufi.2023@gmail.com>
2022-12-24 19:41:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const players = []
+const limbs = ["Левая нога", "Правая нога", "Левая рука", "Правая рука"]
+const colors = ["red", "green", "blue", "yellow"]
+
+const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+function addPlayer () {
+  const input = document.querySelector('#getPlayer')
+  players.push(input.value)
+  input.value = ""
+}
+
+let index = 0
+
+const nextMove = () => {
+  const game = document.querySelector('#game')
+  const player = document.querySelector('#player')
+  const limb = document.querySelector('#limb')
+  
+  game.style.backgroundColor = colors[random(0, colors.length)]
+  index === players.length - 1 ? index = 0 : index++
+  player.innerHTML = players[index]
+  limb.innerHTML = limbs[random(0, limbs.length-1)]
+}
